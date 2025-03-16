5ccaaa64a313a34c30dc6ada466d89b62d72cfb2
Fredrik Bergstrand <fred.bergstrand@gmail.com>
2025-03-16 21:37:29 +0100

    Add function for operate sum and clear calculator display

--- script.js
+++ script.js
@@ -3,18 +3,71 @@ const _SUBTRACT = "-";
 const _MULTIPLY = "*";
 const _DIVIDE = "/";
 
-function add(a, b) {
-  return a + b;
+let a = "";
+let b = "";
+let operator = "";
+let sum = 0;
+
+const display = document.querySelector("#display");
+display.textContent = sum;
+
+const buttons = document.querySelectorAll(".calc-btn");
+buttons.forEach((btn) =>
+  btn.addEventListener("click", (e) => changeDisplay(e, display))
+);
+
+const enterBtn = document
+  .querySelector("#enter")
+  .addEventListener("click", () => getSum(display));
+
+const clearBtn = document
+  .querySelector("#clear")
+  .addEventListener("click", () => clearDisplay(display));
+
+function getSum(display) {
+  console.log("hejsan");
+  if (a === "") return;
+
+  console.log("hej");
+  sum = operate(a, b, operator);
+
+  display.textContent = sum;
+  clearDisplay(display, false);
 }
-function subtract(a, b) {
-  return a - b;
+
+function clearDisplay(display, clearSum = true) {
+  a = "";
+  b = "";
+  operator = "";
+  sum = 0;
+  if (clearSum) {
+    display.textContent = sum;
+  }
 }
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  if (b === 0) return "You bastard! Trying to break me EEEeeeeyyy!!!.......";
-  return a / b;
+
+function changeDisplay(e, display) {
+  let isNr = e.target.id;
+  console.log(!parseInt(isNr));
+  if ((sum === 0 && !parseInt(isNr)) || sum.length == 11) return;
+  if (
+    (operator === "" && parseInt(isNr)) ||
+    (operator === "" && isNr === "0")
+  ) {
+    a = parseInt(`${a}${isNr}`);
+    sum = a;
+    display.textContent = sum;
+  } else if (!parseInt(isNr) && operator === "") {
+    operator = e.target.id;
+    sum = `${a}${operator}`;
+    display.textContent = sum;
+  } else if (parseInt(isNr) || (b !== "" && isNr === "0")) {
+    b = parseInt(`${b}${e.target.id}`);
+    sum = `${a}${operator}${b}`;
+    display.textContent = sum;
+  }
+  console.log("a: " + a);
+  console.log("operator: " + operator);
+  console.log("b: " + b);
 }
 
 function operate(a, b, operator) {
@@ -34,6 +87,20 @@ function operate(a, b, operator) {
       break;
   }
   return sum;
+}
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) return "You bastard! Trying to break me EEEeeeeyyy!!!.......";
+  return a / b;
 }
 
 console.log(operate(2, 5, "+"));
